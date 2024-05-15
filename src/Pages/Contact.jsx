3df7835556fb0307e944eb83fa23dce87a10ba21
Rahmat Arayyan <rahmatarayyan26@gmail.com>
2024-05-15 20:18:04 +0700

    Deployment and Use Redux

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,10 +1,18 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
 import { logout } from "../redux/actions/authActions";
 import { toggleNavbar } from "../redux/reducers/navbarReducers";
 import { ToastContainer } from "react-toastify";
-import { FaUser, FaChevronDown, FaSignOutAlt } from "react-icons/fa";
+import {
+  FaUser,
+  FaChevronDown,
+  FaSignOutAlt,
+  FaPhoneAlt,
+  FaInstagram,
+  FaGithub,
+  FaEnvelope,
+} from "react-icons/fa";
 import {
   setShowDropdown,
   setShowLogoutModal,
@@ -21,7 +29,6 @@ export default function Contact() {
     if (!isLoggedIn) {
       navigate("/login");
     }
-    // Jika belum login, alihkan ke halaman login
   }, [isLoggedIn, navigate]);
 
   const handleToggleNavbar = () => {
@@ -30,7 +37,7 @@ export default function Contact() {
 
   const handleLogout = () => {
     dispatch(logout(navigate));
-    dispatch(setShowLogoutModal()); // tutup modal setelah logout
+    dispatch(setShowLogoutModal());
     dispatch(setShowDropdown(false));
   };
 
@@ -94,9 +101,9 @@ export default function Contact() {
                   <FaChevronDown className="ml-2" />
                 </button>
                 {showDropdown && (
-                  <div className="absolute  mt-2 w-48 bg-white rounded-md shadow-lg py-1 z-20">
+                  <div className="absolute mt-2 w-48 bg-white rounded-md shadow-lg py-1 z-20">
                     <button
-                      className="flex px-4 py-2 text-gray-800 hover:bg-gray-200 w-full text-left items-center "
+                      className="flex px-4 py-2 text-gray-800 hover:bg-gray-200 w-full text-left items-center"
                       onClick={() => dispatch(setShowLogoutModal())}
                     >
                       <FaSignOutAlt className="mr-2" />
@@ -114,112 +121,60 @@ export default function Contact() {
         <h1 className="text-3xl font-bold text-gray-800 mb-4">Contact Us</h1>
         <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
           {/* Card 1 - Phone Number */}
-          <div className="bg-white rounded-lg shadow-md p-6 hover:scale-105 border">
+          <a
+            href="tel:085798160976"
+            className="bg-white rounded-lg shadow-md p-6 hover:scale-105 border block"
+          >
             <div className="flex items-center mb-4">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6 mr-2 text-gray-600"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M12 6v6m0 0v6m0-6h6m-6 0H6"
-                />
-              </svg>
+              <FaPhoneAlt className="h-6 w-6 mr-2 text-gray-600" />
               <p className="text-lg font-medium text-gray-800">Phone Number</p>
             </div>
             <p className="text-gray-600">085798160976</p>
-          </div>
+          </a>
 
           {/* Card 2 - Instagram */}
-          <div className="bg-white rounded-lg shadow-md p-6 hover:scale-105 border">
+          <a
+            href="https://www.instagram.com/rahmatarayyan05/"
+            className="bg-white rounded-lg shadow-md p-6 hover:scale-105 border block"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <div className="flex items-center mb-4">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6 mr-2 text-gray-600"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M12 2a10 10 0 00-2.498 19.601A9.969 9.969 0 0012 22a9.968 9.968 0 002.498-19.601A10 10 0 0012 2zm0 4.969a2.12 2.12 0 100 4.241 2.12 2.12 0 000-4.241z"
-                />
-              </svg>
+              <FaInstagram className="h-6 w-6 mr-2 text-gray-600" />
               <p className="text-lg font-medium text-gray-800">Instagram</p>
             </div>
-            <a
-              href="https://www.instagram.com/rahmatarayyan05/"
-              className="text-blue-500 hover:underline"
-              target="_blank"
-            >
-              @rahmatarayyan05
-            </a>
-          </div>
+            <p className="text-blue-500 hover:underline">@rahmatarayyan05</p>
+          </a>
 
           {/* Card 3 - Github */}
-          <div className="bg-white rounded-lg shadow-md p-6 hover:scale-105 border">
+          <a
+            href="https://github.com/rayyan503"
+            className="bg-white rounded-lg shadow-md p-6 hover:scale-105 border block"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <div className="flex items-center mb-4">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6 mr-2 text-gray-600"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M10.586 4.586a2 2 0 012.828 0L16 8.243V10h-3a2 2 0 00-2 2v4.243l-1.586 1.586a2 2 0 01-2.828 0L4 14.243V10h3a2 2 0 002-2V4.586zM8 14.243V10h3v4.243"
-                />
-              </svg>
+              <FaGithub className="h-6 w-6 mr-2 text-gray-600" />
               <p className="text-lg font-medium text-gray-800">Github</p>
             </div>
-            <a
-              href="https://github.com/rayyan503"
-              className="text-blue-500 hover:underline"
-              target="_blank" // Buka tautan dalam tab baru
-            >
-              @rayyan503
-            </a>
-          </div>
+            <p className="text-blue-500 hover:underline">@rayyan503</p>
+          </a>
 
           {/* Card 4 - Email */}
-          <div className="bg-white rounded-lg shadow-md p-6 hover:scale-105 border">
+          <a
+            href="mailto:rahmatarayan26@gmail.com"
+            className="bg-white rounded-lg shadow-md p-6 hover:scale-105 border block"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <div className="flex items-center mb-4">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6 mr-2 text-gray-600"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M12 14l9-5-9-5-9 5 9 5z"
-                />
-              </svg>
+              <FaEnvelope className="h-6 w-6 mr-2 text-gray-600" />
               <p className="text-lg font-medium text-gray-800">Email</p>
             </div>
-            <p className="text-gray-600">
-              <a
-                href="mailto:rahmatarayan26@gmail.com"
-                className="text-blue-500 hover:underline"
-                target="_blank" // Buka tautan Instagram dalam tab baru
-              >
-                rahmatarayan26@gmail.com
-              </a>
+            <p className="text-blue-500 hover:underline">
+              rahmatarayan26@gmail.com
             </p>
-          </div>
+          </a>
         </div>
       </div>
 
